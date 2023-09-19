5c5376eb11745aeeee6973b1498850337e3f5226
Gianluca <gianluca15.gb@gmail.com>
2023-09-19 18:39:29 -0300

    Criando a view

--- src/views/city_view.py
+++ src/views/city_view.py
@@ -1,11 +1,14 @@
 from argparser import ArgParser
+from datetime import datetime
 from utils import console_clear, get_numeric_input
+
+# TODO: CONFERE TUDO AI, NAO SEI SE TA CERTO
 
 
 class CityView():
     def __init__(self, main_controller, controller):
         self.__main_controller = main_controller
-        self.__controller = controller
+        self.__controller = controller()
 
     def run(self):
         console_clear()
@@ -24,23 +27,72 @@ class CityView():
         option = get_numeric_input(">>> ", 0)
 
         if option == 1:
-            print("CRUD")
+            print("Insert a new city")
+
+            city_id = get_numeric_input("City ID: ", 0) 
+            city = input("City: ")
+            country_id = get_numeric_input("Country ID: ", 0)
+            last_update = datetime.now()
+
+            if (city_id is None or not int) or (city is None or not str) or (country_id is None or not int):
+                self.run()
+
+            print(self.__controller.create(city_id, city, country_id, last_update))
+            input("Press enter to continue...")
             self.run()
 
         elif option == 2:
-            print("CRUD")
+            print("Update a city")
+
+            city_id = get_numeric_input("City ID: ", 0)
+            city = input("City: ")
+            country_id = get_numeric_input("Country ID: ", 0)
+            last_update = datetime.now()
+
+            if (city_id is None or not int) or (city is None or not str) or (country_id is None or not int):
+                self.run()
+            print(self.__controller.update(city_id, city, country_id, last_update))
+            input("Press enter to continue...")
             self.run()
 
         elif option == 3:
-            print("CRUD")
+            print("Delete a city")
+
+            city_id = get_numeric_input("City ID: ", 0)
+
+            if city_id is None or not int:
+                self.run()
+            print(self.__controller.delete(city_id))
+            print("Press enter to continue...")
             self.run()
 
         elif option == 4:
-            print("CRUD")
+            print("Show city data")
+
+            city = input("City: ")
+
+            if city is None or not str:
+                self.run()
+
+            print(self.__controller.read_by_name(city))
+            input("Press enter to continue...")
             self.run()
 
         elif option == 5:
-            print("CRUD")
+            print("List cities in country")
+
+            country = input("Country: ")
+
+            if country is None or not str:
+                self.run()
+
+            list = self.__controller.list_by_country_name(country)
+
+            for city in list:
+                print(city)
+
+            input("Press enter to continue...")
+            
             self.run()
 
         elif option == 6:
